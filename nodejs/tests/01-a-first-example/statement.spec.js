f156de49bdfb00806d887bdf07d126ba976c0a23
jswanr <jswanr@psl.com.co>
2019-05-08 15:11:06 -0500

    Refactor: Change Function Declaration. remove thisAmount parameter from amountFor

--- nodejs/tests/01-a-first-example/statement.spec.js
+++ nodejs/tests/01-a-first-example/statement.spec.js
@@ -2,28 +2,94 @@ const statement = require('../../src/01-a-first-example/statement');
 
 test('create a statemen', () => {
   const invoce = {
-    "customer": "BigCo",
-    "performances": [
+    customer: 'BigCo',
+    performances: [
       {
-        "playID": "hamlet",
-        "audience": 55
+        playID: 'hamlet',
+        audience: 55
       },
       {
-        "playID": "as-like",
-        "audience": 35
+        playID: 'as-like',
+        audience: 35
       },
       {
-        "playID": "othello",
-        "audience": 40
+        playID: 'othello',
+        audience: 40
       }
     ]
   };
 
   const plays = {
-    "hamlet": { "name": "Hamlet", "type": "tragedy" },
-    "as-like": { "name": "As You Like It", "type": "comedy" },
-    "othello": { "name": "Othello", "type": "tragedy" }
+    hamlet: { name: 'Hamlet', type: 'tragedy' },
+    'as-like': { name: 'As You Like It', type: 'comedy' },
+    othello: { name: 'Othello', type: 'tragedy' }
   };
 
   expect(statement(invoce, plays)).toMatchSnapshot();
+});
+
+test('type does not exists', () => {
+  const plays = {
+    avenger: {
+      name: 'Avenger',
+      type: 'science-fiction'
+    }
+  };
+
+  const invoces = {
+    customer: 'New Enterprise',
+    performances: [{
+      playID: 'avenger',
+      audience: 300
+    }]
+  };
+
+  expect(() => statement(invoces, plays)).toThrowErrorMatchingSnapshot();
+});
+
+test('performances with little audience', () => {
+  const plays = {
+    hamlet: { name: 'Hamlet', type: 'tragedy' },
+    'as-like': { name: 'As You Like It', type: 'comedy' },
+    othello: { name: 'Othello', type: 'tragedy' }
+  };
+
+  const invoices = {
+    customer: 'Alternative Performance',
+    performances: [
+      {
+        playID: 'as-like',
+        audience: 15
+      },
+      {
+        playID: 'othello',
+        audience: 25
+      }
+    ]
+  };
+
+  expect(statement(invoices, plays)).toMatchSnapshot();
+});
+
+test('performances with minimal audience', () => {
+  const plays = {
+    hamlet: { name: 'Hamlet', type: 'tragedy' },
+    'as-like': { name: 'As You Like It', type: 'comedy' },
+    othello: { name: 'Othello', type: 'tragedy' }
+  };
+
+  const invoices = {
+    customer: 'Alternative Performance',
+    performances: [
+      {
+        playID: 'as-like',
+        audience: 20
+      },
+      {
+        playID: 'othello',
+        audience: 30
+      }]
+  };
+
+  expect(statement(invoices, plays)).toMatchSnapshot();
 });
