1a6877ba1bb9c057a67500d0973207c46ff25446
jswanr <jswanr@psl.com.co>
2019-05-08 15:33:31 -0500

    Refactor: Change function declaration. Remove play variable from amountFor

--- nodejs/src/01-a-first-example/statement.js
+++ nodejs/src/01-a-first-example/statement.js
@@ -9,22 +9,23 @@ function statement(invoice, plays) {
       minimumFractionDigits: 2
     });
   for (const perf of invoice.performances) {
-    const play = plays[perf.playID];
-
-    thisAmount = amountFor(play, perf);
-
+    thisAmount = amountFor(playFor(perf), perf);
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5);
+    if (playFor(perf).type === 'comedy') volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+    result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
+
+  function playFor(perf) {
+    return plays[perf.playID];
+  }
 
   function amountFor(aPlay, aPerformance) {
     let amountResult = 0;
