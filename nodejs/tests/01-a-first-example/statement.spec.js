0cc7e89b15308c7b0fb42e34792acb9bc402c464
Alejandro Perdomo <aperdomobo@gmail.com>
2019-05-08 09:04:09 -0500

    add files

---
+++ nodejs/tests/01-a-first-example/statement.spec.js
@@ -0,0 +1,29 @@
+const statement = require('../../src/01-a-first-example/statement');
+
+test('create a statemen', () => {
+  const invoce = {
+    "customer": "BigCo",
+    "performances": [
+      {
+        "playID": "hamlet",
+        "audience": 55
+      },
+      {
+        "playID": "as-like",
+        "audience": 35
+      },
+      {
+        "playID": "othello",
+        "audience": 40
+      }
+    ]
+  };
+
+  const plays = {
+    "hamlet": { "name": "Hamlet", "type": "tragedy" },
+    "as-like": { "name": "As You Like It", "type": "comedy" },
+    "othello": { "name": "Othello", "type": "tragedy" }
+  };
+
+  expect(statement(invoce, plays)).toMatchSnapshot();
+});
